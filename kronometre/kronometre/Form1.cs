668f5a0bda38865f6d3b685e94590e741329e80f
Mikdats <96742937+Mikdats@users.noreply.github.com>
2022-01-19 18:33:01 +0300

    Add files via upload

---
+++ kronometre/kronometre/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label1.Text);
+            sayi++;
+            label1.Text = sayi.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(label1.Text);
+            sayi = 0;
+            label1.Text = sayi.ToString();
+        }
+    }
+}
